d77607df4a43c0399035bea1318b285ebcf9471b
NataliaSantos471 <nataliasanleo95@gmail.com>
2020-12-11 15:58:46 -0500

    se agrega json

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script>
-    export default {
-        name: "TeamCard",
-        props: ['member']
-    }
+export default {
+  codigo: 1,
+  nombre: "Lucas Mera",
+  descripcion:
+    "Lorem ipsum dolor sit amet, consectetur adipiscinglit. Mauris condimentum ac elit et accumsan.",
+  rol: "Desarrollador backend",
+  image: "path/to/image",
+};
 </script>
 
 <style scoped>
-
 </style>
